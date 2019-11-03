d196c7ca071c1f263bb06ab9ae9cccfe691c95e0
AnaSundji <ana.sundji@fer.hr>
2019-11-03 23:58:32 +0100

    Napravljena klasa za reprezentaciju pitanja i klasa za odgovor. Započet rad na skriptama za korisnika.

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class User
+{
+
+    public string Username;
+    public string Password;
+    public string Mail;
+
+    public int Score;
+
+    // public Achievement[] Achievements;
+    // public Lesson[] Lessons;
+    // public Target[] ScannedTargets;
+
+    // Achievement klasa bi sadržavala: naziv, opis (opcionalno, pr. ako se klikne da pokaže opis), sliku (opcionalno, ovisi hoćemo li na serveru spremati)
+    // Lesson klasa bi sadržavala: naziv, status (završeno ili ne), najbolji rezultat na testu, broj slika koje nisu skenirane (da znamo pratiti status)
+    // Target klasa bi sadržavala: naziv ili neku vrstu ID-a, status (skenirano ili ne)
+
+}
