5d15a00c44d3e3a2a90a6ce4e8c39ab5215ad3f5
AnaSundji <ana.sundji@fer.hr>
2019-11-04 16:09:06 +0100

    Dodane skripte za izradu datoteke s pitanjima za pojedinu lekciju preko Unity editora.

---
+++ Assets/Scripts/Quiz Scripts/QuestionDatabaseCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using UnityEngine;
+
+public class QuestionDatabaseCreator : MonoBehaviour
+{
+
+    public string DatabaseLessonName;
+    public QuestionData[] DatabaseQuestions;
+
+    private string _path;
+
+    private void Awake()
+    {
+        _path = Application.dataPath + "/StreamingAssets/";
+    }
+
+    public void SaveQuestionDatabase()
+    {
+
+        if (DatabaseLessonName.Length == 0)
+            return;
+
+        QuestionDatabase database = new QuestionDatabase
+        {
+            LessonName = DatabaseLessonName,
+            Questions = DatabaseQuestions
+        };
+
+        string json = JsonUtility.ToJson(database);
+        File.WriteAllText(_path + DatabaseLessonName + ".json", json);
+        
+    }
+
+}
---
+++ Assets/Scripts/Quiz Scripts/QuestionDatabaseCreatorEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(QuestionDatabaseCreator))]
+
+public class QuestionDatabaseCreatorEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Save"))
+            ((QuestionDatabaseCreator)target).SaveQuestionDatabase();
+
+    }
+
+}
