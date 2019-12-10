468418c72a137f834cda1ff6dabcc74e54143d4e
NikolaVuk <nikola.vugdelija@fer.hr>
2019-12-10 17:27:35 +0100

    dodana rotacija pomoću swipe-a i scale-anje pomoću pinch-a

--- Assets/Scripts/AR Scripts/ObjectController.cs
+++ Assets/Scripts/AR Scripts/ObjectController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class ObjectController : MonoBehaviour
 {
-
+    private Touch touch;
     public float rotationSpeed = 70;
     public float scalingSpeed = 3;
+    public float maxVelicina;
+    public float minVelicina;
 
     private Transform body;
 
@@ -19,39 +21,43 @@ public class ObjectController : MonoBehaviour
     private void Update()
     {
 
-        if(Input.GetKey("i"))
+        if (Input.touchCount == 1)
         {
-            this.Scale(scalingSpeed );
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                float rotacijax = touch.deltaPosition.x * rotationSpeed * Mathf.Deg2Rad;
+                float rotacijay = touch.deltaPosition.y * rotationSpeed * Mathf.Deg2Rad;
+                transform.Rotate(rotacijay, -rotacijax, 0, Space.World);
+                //transform.Rotate(Vector3.right, rotacijay);
+            }
         }
 
-        if (Input.GetKey("o"))
+        if (Input.touchCount == 2)
         {
-            this.Scale(-scalingSpeed );
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+
+            Vector2 stariTouch1 = touch1.position - touch1.deltaPosition;
+            Vector2 stariTouch2 = touch2.position - touch2.deltaPosition;
+
+            float staraUdaljenost = (stariTouch1 - stariTouch2).magnitude;
+            float sadasnjaUdaljenost = (touch1.position - touch2.position).magnitude;
+
+            float razlika = sadasnjaUdaljenost - staraUdaljenost;
+
+            Scale(razlika * 0.1f);
         }
 
-        if(Input.GetKey("d"))
-        {
-            this.Rotate(rotationSpeed );
-        }
-
-        if (Input.GetKey("a"))
-        {
-            this.Rotate(-rotationSpeed );
-        }
-
-    }
-
-    public void Rotate(float value)
-    {
-        body.Rotate(0, value * Time.deltaTime, 0);
     }
 
     public void Scale(float value)
     {
-        if ((body.localScale.x > 0.01 && value<0) || (body.localScale.x < 0.9 && value > 0))
+        if ((body.localScale.x > minVelicina && value < 0) || (body.localScale.x < maxVelicina && value > 0))
         {
-            body.localPosition += new Vector3(0, value/2*Time.deltaTime, 0);
-            body.localScale += new Vector3(value * Time.deltaTime, value*Time.deltaTime, value * Time.deltaTime);
+            body.localPosition += new Vector3(0, value / 2 * Time.deltaTime, 0);
+            body.localScale += new Vector3(value * Time.deltaTime, value * Time.deltaTime, value * Time.deltaTime);
         }
     }
 
