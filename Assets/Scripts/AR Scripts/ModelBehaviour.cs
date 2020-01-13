3bfe5f6f9d220159b3315dba3056fe2d8bf1b44e
AnaSundji <ana.sundji@fer.hr>
2020-01-14 00:59:54 +0100

    Popravljen raspored scena za build. Dodana skripta za pozivanje eventa kad se model pozove. Namješteni modeli u AR sceni.

---
+++ Assets/Scripts/AR Scripts/ModelBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ModelBehaviour : MonoBehaviour
+{
+
+    public static CustomEvent<string, string, string> ModelVisibleEvent = new CustomEvent<string, string, string>();
+    public static UnityEvent ModelInvisibleEvent = new UnityEvent();
+
+    public string Model;
+    public string Lesson;
+    public string About;
+
+    private void OnBecameVisible()
+    {
+        ModelVisibleEvent.Invoke(Model, Lesson, About);
+        Debug.Log("Model " + Model + " visible...");
+    }
+
+    private void OnBecameInvisible()
+    {
+        ModelInvisibleEvent.Invoke();
+        Debug.Log("Model " + Model + " invisible...");
+    }
+
+}
